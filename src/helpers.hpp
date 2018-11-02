9f0fd76598e23930d5c649e514a1cc7ccb5c915e
Safina Marina <tigra_84@mail.ru>
2018-11-02 10:46:20 +0300

    move method to helpers

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -43,3 +43,23 @@ struct VectorArchive: fsdk::IArchive
 	{}
 };
 
+// only for depth test
+fsdk::Image loadImage(const char* name) {
+	std::ifstream file(name, std::ios::in|std::ios::binary);
+	
+	int channels = 0;
+	int elementSize = 0;
+	int rows = 0;
+	int cols = 0;
+	
+	file.read((char*)&channels, sizeof(channels));
+	file.read((char*)&elementSize, sizeof(elementSize));
+	file.read((char*)&rows, sizeof(rows));
+	file.read((char*)&cols, sizeof(cols));
+	
+	fsdk::Image image(cols, rows, fsdk::Format::R16);
+	
+	file.read((char*)image.getData(), rows * cols * channels * elementSize);
+	
+	return image;
+};
