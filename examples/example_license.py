4b97946668efcd9328a7c92750021238f43e30a0
s.koltsov <s.koltsov@visionlabs.ru>
2019-04-17 11:48:19 +0300

    License - detector creation check.

--- examples/example_license.py
+++ examples/example_license.py
@@ -5,7 +5,7 @@ sys.path.append(sys.argv[1])
 import FaceEngine as fe
 
 def help():
-    print("python example_license.py <path to FaceEngine*.so> <path to license.conf>")
+    print("python example_license.py <path to FaceEngine*.so> <path to the folder with license.conf>")
 
 def get_info():
     print(fe.getVersionHash())
@@ -15,9 +15,9 @@ def get_info():
 def checkLicense(license):
 
     if license.isActivated():
-        print("License if activated!")
+        print("License is activated!")
     else:
-        print("License if NOT activated!")
+        print("License is NOT activated!")
 
     freatures = [
         fe.LicenseFeature.Detection,
@@ -39,6 +39,8 @@ def checkLicense(license):
         else:
             print("{0} is NOT available".format(feature))
 
+    return license.isActivated()
+
 if __name__ == "__main__":
     # correct path or put directory "data" with example.py
     faceEngine = fe.createFaceEngine(
@@ -55,11 +57,23 @@ if __name__ == "__main__":
     try:
         license = faceEngine.getLicense()
 
-        checkLicense(license)
+        if not checkLicense(license):
+            # check facility for not activated license
+            detector = faceEngine.createDetector(fe.FACE_DET_V1)
+            if detector is None:
+                print("Detector creation failed! License is not activated!")
 
-        faceEngine.activateLicense(license, licenseConfPath)
 
-        checkLicense(license)
+        if faceEngine.activateLicense(license, licenseConfPath):
+            print("License was sucessfully activated!")
+        else:
+            print("License activation failed! See debug logs for details.")
+
+        if checkLicense(license):
+            # check facility for activated license
+            detector = faceEngine.createDetector(fe.FACE_DET_V1)
+            if detector is not None:
+                print("Detector creation success! License is activated!")
 
     except Exception as ex:
         print(type(ex).__name__, ex)
