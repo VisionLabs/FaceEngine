15f01a4c2f042e3c7673fc107b7d1aeeca6ca0f4
Lesha <a.balashov@visionlabs.ru>
2019-09-04 17:29:20 +0300

    depth example

--- examples/example_depth.py
+++ examples/example_depth.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import cv2
+import numpy as np
 
 #Parse arguments and load additional modules
 parser = argparse.ArgumentParser(description = 'Luna SDK realsense depth demo')
@@ -16,6 +17,7 @@ import FaceEngine as fe
 import pyrealsense2 as rs
 
 if __name__ == "__main__":
+	#create Face Engine root object
 	root = fe.createFaceEngine(args.data)
 	#check license
 	license = root.getLicense()
@@ -24,6 +26,91 @@ if __name__ == "__main__":
 		exit(-1)
 	
 	detector = root.createDetector(fe.FACE_DET_V1)
+	estimator = root.createDepthEstimator()
 	warper = root.createWarper()
 	
+	# Configure streams and start the pipeline
+	pipe = rs.pipeline()
+	pipeProfile = pipe.start()
 
+	device = pipeProfile.get_device()
+	print(device)
+
+	# Since realsense depth sensors contain pixel values in hardware specific units
+	# we have to manually convert it to milimeters for depth estimator to work properly
+	depthSensor = device.first_depth_sensor()
+	depthScale = depthSensor.get_depth_scale() * 1000
+	print("Depth scale is {}".format(depthScale))
+
+	#For warper to work properly, we need to align both stream viewports.
+	align_to = rs.stream.color
+	depthToColorAlignment = rs.align(align_to)
+
+	#create window
+	cv2.namedWindow('Realsense depth example', cv2.WINDOW_AUTOSIZE)
+	
+	# main processing loop
+	try:
+		while True:
+			# Get frameset of color and depth
+			frames = pipe.wait_for_frames()
+	
+			# Align the depth frame to color frame
+			alignedFrames = depthToColorAlignment.process(frames)
+	
+			# Get aligned frames
+			depthFrame = alignedFrames.get_depth_frame()
+			rgbFrame = alignedFrames.get_color_frame()
+
+			# Validate that both frames are valid
+			if not depthFrame or not rgbFrame:
+				continue
+		
+			#convert depth map to milimeters
+			depthFrame = np.asanyarray(depthFrame.get_data())
+			depthFrame = (depthFrame * depthScale).astype(np.int16)
+			
+			rgbFrame = np.asanyarray(rgbFrame.get_data())
+			
+			#convert incoming frames to SDK images
+			depthImage = fe.Image()
+			depthImage.setData(depthFrame, fe.FormatType.R16)
+
+			rgbImage = fe.Image()
+			rgbImage.setData(rgbFrame, fe.FormatType.R8G8B8)
+
+			#perform detection
+			err, face = detector.detectOne(rgbImage, rgbImage.getRect(), fe.DetectionType(fe.dt5Landmarks))
+#			print(face.detection)
+			
+			cvRGBImage = cv2.cvtColor(rgbFrame, cv2.COLOR_BGR2RGB)
+			if(err.isOk and face.isValid()):
+				box = face.detection.rect
+				cv2.rectangle(cvRGBImage, (int(box.x), int(box.y)), (int(box.x + box.width), int(box.y + box.height)), (0,255,0), 2)
+
+				#warp depth image
+				transformation = warper.createTransformation(face.detection, face.landmarks5_opt.value())
+				warpResult, warpImage = warper.warp(depthImage, transformation)
+				if warpResult.isError:
+					print("Failed to warp image!")
+					continue
+				
+				error, output = estimator.estimate(warpImage)
+				if(error.isOk):
+					color = (0, 0, 255)
+					if(output.isReal):
+						color = (0, 255, 0)
+					cv2.rectangle(cvRGBImage,
+						(int(box.x), int(box.y)),
+						(int(box.x + box.width), int(box.y + box.height)),
+						color, 2)
+					print(output)
+
+			cv2.imshow('Realsense depth example', cvRGBImage)
+			key = cv2.waitKey(1)
+			# Press esc or 'q' to close the image window
+			if key & 0xFF == ord('q') or key == 27:
+				cv2.destroyAllWindows()
+				break
+	finally:
+		pipe.stop()
