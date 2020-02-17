8afcfdc6dd83b6059f47c151b8ce88723d7e9bb7
s.koltsov <s.koltsov@visionlabs.ru>
2020-02-17 13:53:42 +0300

    [FSDK-2031] [FSDK-2104] docs - Troubleshooting and diagnostics.

--- src/LivenessEngineAdapter.cpp
+++ src/LivenessEngineAdapter.cpp
@@ -13,20 +13,20 @@ PyILivenessEngine::PyILivenessEngine(
 	const char* dataPath) {
 	livenessEnginePtr = fsdk::acquire(lsdk::createLivenessEngine(pyIFaceEngine.faceEnginePtr, dataPath));
 	if (!livenessEnginePtr)
-		throw py::cast_error("\nFailed to create livenessEnginePtr instance! VERIFY PATH to \"data\" directory!");
+		throw py::cast_error("\nFailed to create livenessEnginePtr instance! See the \"Troubleshooting and diagnostics\" chapter in the documentation for possible reasons.");
 }
 
 lsdk::ILivenessPtr PyILivenessEngine::createLiveness(lsdk::LivenessAlgorithmType type) {
 	lsdk::ILivenessPtr livenessPtr = fsdk::acquire(livenessEnginePtr->createLiveness(type));
 	if (!livenessPtr)
-		throw py::cast_error("\nFailed to create liveness instance! VERIFY PATH to \"data\" directory!");
+		throw py::cast_error("\nFailed to create liveness instance! See the \"Troubleshooting and diagnostics\" chapter in the documentation for possible reasons.");
 	return livenessPtr;
 }
 
 lsdk::IComplexLivenessPtr PyILivenessEngine::createComplexLiveness(lsdk::ComplexLivenessAlgorithmType type) {
 	lsdk::IComplexLivenessPtr livenessPtr = fsdk::acquire(livenessEnginePtr->createComplexLiveness(type));
 	if (!livenessPtr)
-		throw py::cast_error("\nFailed to create complex liveness instance! VERIFY PATH to \"data\" directory!");
+		throw py::cast_error("\nFailed to create complex liveness instance! See the \"Troubleshooting and diagnostics\" chapter in the documentation for possible reasons.");
 	return livenessPtr;
 }
 
