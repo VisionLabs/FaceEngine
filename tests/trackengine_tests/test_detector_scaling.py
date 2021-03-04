ac29aae176ed615613d051e254fa7192e0fa1d24
Alexande Ganushchak <a.ganushchak@visionlabs.ru>
2021-03-05 01:51:21 +0300

    split into separate files

---
+++ tests/trackengine_tests/test_detector_scaling.py
@@ -0,0 +1,39 @@
+from trackengine_tests.trackengine_base_class import TestTrackEngine
+from trackengine_tests.trackengine_base_class import change_value_in_trackengine_conf
+import time
+
+import FaceEngine as fe
+import TrackEngine as te
+
+class TestTrackEngineDetectorScaling(TestTrackEngine):
+
+    def test_detector_scaling(self):
+        cases = (
+            {'scale_size': "640", "frames": 5, "expected": 5},
+            {'scale_size': "240", "frames": 5, "expected": 0}
+        )
+        for case in cases:
+            with self.subTest(case=case):
+                change_value_in_trackengine_conf("detector-scaling", "x", "1", section_name="other")
+                change_value_in_trackengine_conf("scale-result-size", "x", case['scale_size'], section_name="other")
+                self.trackengine = te.createTrackEngine(self.faceEngine, "data/trackengine.conf")
+                image_full_hd_path = 'testData/image_1080_1_face.jpg'
+                image_full_hd = fe.Image()
+                err_image_loaded = image_full_hd.load(image_full_hd_path)
+                self.assertTrue(err_image_loaded.isOk)
+                stream = self.trackengine.createStream()
+                print('Stream created!')
+                for x in range(0, case['frames']):
+                    while not (stream.pushFrame(image_full_hd, x)):
+                        time.sleep(0.01)
+                    print("pushed {0}".format(x), flush=True)
+                print("All frames are pushed")
+                stream.waitStream()
+                clb = stream.getCallbacks()
+                bestshots = 0
+                for c in clb:
+                    if c.type.name == 'ctBestShot':
+                        bestshots += 1
+                    print(c.type)
+                    print(c.bbox)
+                self.assertEqual(bestshots, case['expected'], "Number of besthots dont match the number of frames.")
---
+++ tests/trackengine_tests/trackengine_base_class.py
@@ -0,0 +1,47 @@
+import unittest
+import argparse
+import sys
+import os
+import xml.etree.cElementTree as ET
+import time
+
+from license_helper import make_activation, ActivationLicenseError
+
+
+
+# if FaceEngine is installed only this string of code is required for module importing
+import FaceEngine as fe
+import TrackEngine as te
+
+
+def change_value_in_trackengine_conf(param_name, key, new_value, section_name):
+    tree = ET.ElementTree(file='data/trackengine.conf')
+    elem = tree.find(".//*[@name='{0}']/param[@name='{1}']".format(section_name, param_name))
+    if elem is None:
+        print("{0} in {1} not found".format(param_name, section_name))
+        exit(1)
+    if elem.get(key) is None:
+        print("{0} not found in {1}".format(key, param_name))
+        exit(1)
+    elem.set(key, new_value)
+    tree.write('data/trackengine.conf', "UTF-8")
+
+
+class TestTrackEngine(unittest.TestCase):
+    faceEngine = None
+    trackEngine = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.faceEngine = fe.createFaceEngine("data")
+        if not make_activation(cls.faceEngine):
+            raise ActivationLicenseError("License is not activated!")
+        cls.image = fe.Image()
+
+    def setUp(self):
+        self.configPath = os.path.join("data", "faceengine.conf")
+        self.config = fe.createSettingsProvider(self.configPath)
+        #при frg-subtractor=1 в детектор отправляются только области с движением
+        change_value_in_trackengine_conf('frg-subtractor', 'x', "0", section_name="other")
+        change_value_in_trackengine_conf("detector-scaling", "x", "0", section_name="other")
+        #change_value_in_trackengine_conf('use-preprocessing-thread', 'x', "0", section_name="other")
